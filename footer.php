e8c1fc0162dc3498c1806e54931add15cc4e14f5
EL-J Diaz <eljdiaz500@gmail.com>
2023-05-26 00:57:56 +0800

    Created Footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+  <img src="logo.png" alt="Logo">
+  <div class="vertical-line"> </div>
+  <p> Address </p>
+  <p> Unit 01 Building A, Manuel A. Roxas Highway, Clark Freeport Zone, Angeles City, Pampanga </p>
+  <p> Opening Hours </p>
+  <p> Mon - Fri: 11am - 10pm </p>
+  <p> Sat - Sun: 11am - 12am </p>
+  <p> Contact Us </p>
+  <p> 123-456-7890 </p>
+  <p> koiramen@gmail.com </p>
+  <p>&copy; 2023 by Koi Ramen. Proudly created by Elmalia Jane Diaz, Joshua Benneth Daguio, Lhizel Tabual and Paulo Bondoc.</p>
+</footer>
