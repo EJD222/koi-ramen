c7c3a51a656d1651739c6d2e36075b2155993411
EL-J Diaz <eljdiaz500@gmail.com>
2023-05-25 23:54:52 +0800

    Page Template

---
+++ menu-bar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+      <li><a href="index.php"><img src="home.png" alt="Home"></a></li>
+      <li><a href="menu-ramen.php">Menu</a></li>
+      <li><a href="about-us.php">About Us</a></li>
+      <li><a href="contact-us.php">Contact Us</a></li>
+      <li><a href="book-now.php">Book Now</a></li>
+    </ul>
+</nav>
