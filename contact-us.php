bae0f44af9f361497cb92d8e5a4437699a247937
EL-J Diaz <eljdiaz500@gmail.com>
2023-05-26 00:56:28 +0800

    Added Footer

--- contact-us.php
+++ contact-us.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contact Us</title>
 </head>
 <body>
-    
+
+    <?php 
+        //Call Menu Bar
+        include 'menu-bar.php'; 
+    ?> 
+
+
+    <?php 
+        //Call Footer
+        include 'footer.php'; 
+    ?> 
+
 </body>
 </html>
--- menu-ramen.php
+++ menu-ramen.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Menu</title>
 </head>
 <body>
     
     <?php 
         //Call Menu Bar
         include 'menu-bar.php'; 
+    ?> 
+
+    <?php 
+        //Call Footer
+        include 'footer.php'; 
     ?> 
 
 </body>
