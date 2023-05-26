63219126288090ac0024d5f80c977ca26a05ea7c
EL-J Diaz <eljdiaz500@gmail.com>
2023-05-26 21:45:51 +0800

    Added CSS

--- menu-bar.php
+++ menu-bar.php
@@ -1,9 +1,24 @@
-<nav>
-    <ul>
-      <li><a href="index.php"><img src="home.png" alt="Home"></a></li>
-      <li><a href="menu-ramen.php">Menu</a></li>
-      <li><a href="about-us.php">About Us</a></li>
-      <li><a href="contact-us.php">Contact Us</a></li>
-      <li><a href="book-now.php">Book Now</a></li>
-    </ul>
-</nav>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter&family=Syncopate:wght@700&family=Syne:wght@600;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="menu-bar-style.css">
+
+<div class = "menu-bar">
+  <nav>
+    <a href="index.php"><img src="images/koi-ramen-logo.png" alt="Home" ></a>  
+      <div class = "menu-links">
+        <ul>
+          <li><a href="menu-ramen.php">MENU</a></li>
+          <li><a href="about-us.php">ABOUT US</a></li>
+          <li><a href="contact-us.php">CONTACT US</a></li>
+          <li><a href="book-now.php">BOOK NOW</a></li>
+        </ul>
+      </div>
+  </nav>
+</div>
+
