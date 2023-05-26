19c3ef0b65d60f96f993d3d08eaa68c9dcc870f1
EL-J Diaz <eljdiaz500@gmail.com>
2023-05-26 22:01:38 +0800

    Added HTML

--- index.php
+++ index.php
@@ -4,14 +4,153 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Syncopate:wght@700&family=Syne:wght@600;700&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="style.css">
+
     <title>Home</title>
 </head>
 <body>
+    <div class="guide">
+        <div class="child">
+            <?php 
+                //Call Menu Bar
+                include 'menu-bar.php'; 
+            ?> 
 
-    <?php 
-        //Call Menu Bar
-        include 'menu-bar.php'; 
-    ?> 
+            <p>experience noodle nirvana!</p>
+            <p>KOI RAMEN</p>
+            <img src = "/images/ramen-bar.jpg" alt="Ramen Bar" width="700" height="500"/>
+            <img src = "/images/center-piece.png" alt="Ramen" width="500" height="500"/>
+
+            <p>Authentic</p>
+            <img src = "/images/authentic.png" alt="Authentic" width="100" height="100"/>
+            <div class="horizontal-line-1"></div>
+            <p>Flavorful</p>
+            <img src = "/images/flavorful.png" alt="Flavorful" width="100" height="100"/>
+            <div class="horizontal-line-2"></div>
+            <p>Quality</p>
+            <img src = "/images/quality.png" alt="Quality" width="100" height="100"/>
+
+            <p>Indulge in the Artistry of <br />Flavor at Koi Ramen: <br />Where Culinary Mastery <br />Meets Noodle Bliss!</p>
+            <p>Experience the exquisite fusion of culinary mastery <br />
+            and noodle bliss at Koi Ramen, where every bowl is a <br />
+            celebration of artistry and flavor, tantalizing your <br />
+            taste buds with each savory spoonful. Discover the <br />
+            harmonious fusion of artistry and noodle perfection <br />
+            that awaits you at our esteemed establishment. <br />
+            <img src = "/images/wave.png" alt="Quality" width="280" height="160"/>
+            <img src = "/images/black-garlic-tonkotsu-ramen.png" alt="Quality" width="360" height="280"/>
+            <img src = "/images/chopsticks2.png" alt="Quality" width="280" height="160"/>
+            <button type="button">About Us</button>
+
+            <img src = "/images/wave2.png" alt="Quality" width="1000" height="20"/>
+            <p>RAMEN CHRONICLES</p>
+            <p>“Koi Ramen has taken the Philippine <br />
+                food scene by storm, earning accolades <br />
+                from local food magazines like Flavors <br />
+                of Manila. Their authentic ramen bowls <br />
+                are a gastronomic delight that keeps  <br />
+                patrons coming back for more.” <br />
+            </p>
+            <p>— Maria Santos, Flavors <br />
+            of Manila Magazine
+            </p>
+            <p>“Food Explorer Magazine raves about <br />
+                Koi Ramen, the new ramen sensation <br />
+                captivating taste buds across the <br />
+                Philippines. From the rich broths to the <br />
+                perfectly cooked noodles, Koi Ramen <br />
+                delivers an unforgettable dining <br />
+                experience.” <br />
+            <p>— Carlos Gomez, Food <br />
+            Explorer Magazine 
+            </p>
+            <p>"Renowned food critic Juanita <br />
+                Fernandez applauds Koi Ramen's artful <br />
+                approach to ramen. The harmonious <br />
+                blend of flavors and top-notch <br />
+                ingredients make Koi Ramen a standout <br />
+                destination for ramen enthusiasts." <br />
+            </p>
+            <p>— Juanita Fernandez, <br />
+            Food Critic <br />
+            </p>
+
+            <p> SIGNATURE BOWLS </p>
+            <img src = "/images/koi.png" alt="Koi Circle" width="400" height="450"/>
+            <img src = "/images/hokkaido-miso-ramen.png" alt="Hokkaido Miso Ramen" width="300" height="300"/>
+            <img src = "/images/spicy-beef-ramen.png" alt="Spicy Beef Ramen" width="200" height="200"/>
+            <img src = "/images/black-garlic-chicken-ramen.png" alt="Black Garlic Chicken Ramen" width="200" height="200"/>
+            <img src = "/images/arrow.png" alt="Arrow" width="100" height="88"/>
+            <p>Hokkaido Miso Ramen</p>
+            <img src = "/images/arrow.png" alt="Arrow" width="100" height="88"/>
+            <p>A rich and hearty noodle soup with <br />
+            a savory miso broth, topped with <br />
+            various ingredients. <br />
+            </p>
+            <p>P500</p>
+            <button type="button">MENU</button>
+            
+
+            <img src = "/images/wave4.png" alt="Wave 4" width="750" height="250"/>
+            <img src = "/images/night-market.jpg" alt="Wave 4" width="600" height="400"/>
+            <img src = "/images/wave4.png" alt="Wave 4" width="750" height="250"/>
+            <p>"Reach Out and Let Your <br />
+                Ramen Cravings Be <br />
+                Heard - We're Here to <br />
+                Listen and Serve You! <br />
+            </p>
+            
+            <p>Thank you for your interest in Koi Ramen! We <br />
+            value your feedback and are excited to hear from <br />
+            you. Whether you have a question, comment, or suggestion, <br />
+            please feel free to reach out to us. <br />
+            Our dedicated team is here to assist you in any <br />
+            way we can. <br />
+            </p>
+            <button type="button">CONTACT US</button>
+
+            <p>“Ramen: A culinary <br />
+                masterpiece that <br />
+                warms the heart and <br />
+                satisfies the soul.” <br />
+            </p>
+            <img src = "/images/scallion3.png" alt="Scallion 3" width="20" height="20"/>
+            <img src = "/images/scallion1.png" alt="Scallion 1" width="20" height="20"/>
+            <img src = "/images/scallion4.png" alt="Scallion 4" width="20" height="20"/>
+            <img src = "/images/scallion2.png" alt="Scallion 2" width="20" height="20"/>
+            <img src = "/images/scallion3.png" alt="Scallion 3" width="20" height="20"/>
+            <img src = "/images/wave6.png" alt="Wave 6" width="480" height="221"/>
+            <img src = "/images/creamy-ramen.png" alt="Cream Ramen" width="80" height="47"/>
+
+            <marquee><p>Ramen Magic,</p> <p>made inclsuive!</p></marquee>
+
+            <img src = "/images/chef.jpg" alt="Chef" width="663" height="994"/>
+
+            <p>Reserve Your Spot for a Memorable 
+            Ramen Experience!
+            </p>
+            <button type="button">BOOK NOW</button>
+            <img src = "/images/lantern.jpg" alt="Lantern" width="200" height="300"/>
+            <img src = "/images/eating-ramen.jpg" alt="Eating Ramen" width="200" height="300"/>
+            <img src = "/images/chef2.jpg" alt="Chef 2" width="450" height="300"/>
+
+            <?php 
+                //Call Footer
+                include 'footer.php'; 
+            ?> 
+
+            <p>
+            </p>
+
+        </div>
+    </div>
+
+
 
 </body>
 </html>
