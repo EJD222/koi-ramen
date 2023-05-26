85db992744177b1446bdf6acb42bce76af6610eb
EL-J Diaz <eljdiaz500@gmail.com>
2023-05-27 01:03:09 +0800

    Added CSS

--- footer.php
+++ footer.php
@@ -1,13 +1,41 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Syncopate:wght@700&family=Syne:wght@600;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="footer-style.css">
+
 <footer>
-  <img src="logo.png" alt="Logo">
-  <div class="vertical-line"> </div>
-  <p> Address </p>
-  <p> Unit 01 Building A, Manuel A. Roxas Highway, Clark Freeport Zone, Angeles City, Pampanga </p>
-  <p> Opening Hours </p>
-  <p> Mon - Fri: 11am - 10pm </p>
-  <p> Sat - Sun: 11am - 12am </p>
-  <p> Contact Us </p>
-  <p> 123-456-7890 </p>
-  <p> koiramen@gmail.com </p>
-  <p>&copy; 2023 by Koi Ramen. Proudly created by Elmalia Jane Diaz, Joshua Benneth Daguio, Lhizel Tabual and Paulo Bondoc.</p>
+  <div class="footer-container">
+    <img src="images/koi-ramen-logo.png" alt="Logo">
+    <div class="vertical-line"></div>
+    <div class="content">
+
+      <div class="details">
+        <div class="address">
+          <p class ="title-of-details"> Address </p>
+          <p class ="content-details"> Unit 01 Building A, Manuel A. Roxas Highway, Clark Freeport Zone, Angeles City, Pampanga </p>
+        </div>
+        <div class="opening-hours">
+          <p class ="title-of-details"> Opening Hours </p>
+          <p class ="content-details"> Mon - Fri: 11am - 10pm </p>
+          <p class ="content-details"> Sat - Sun: 11am - 12am </p>
+        </div>
+        <div class="contact-us">
+          <p class ="title-of-details"> Contact Us </p>
+          <p class ="content-details"> 0912-345-6780 </p>
+          <p class ="content-details"> koiramen@gmail.com </p>
+        </div>
+      </div>
+
+      <div class="copyright">
+        <p class ="content-details">&copy; 2023 by Koi Ramen. Proudly created by Elmalia Jane Diaz, Joshua Benneth Daguio, Lhizel Tabual and Paulo Bondoc.</p>
+      </div>
+
+    </div>
+
+  </div>
 </footer>
